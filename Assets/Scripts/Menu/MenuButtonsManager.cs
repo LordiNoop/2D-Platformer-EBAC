1a9c34568ba14ad371885c3f994bd9773e1d433a
César <cesar.romero.borges@gmail.com>
2025-04-17 17:08:51 -0300

    Add menu and Iterate the Game Manager

---
+++ Assets/Scripts/Menu/MenuButtonsManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MenuButtonsManager : MonoBehaviour
+{
+    public List<GameObject> buttons;
+
+    [Header("Animation")]
+    public float duration = 0.2f;
+    public float delay = 0.05f;
+    public Ease ease = Ease.OutBack;
+
+    private void Awake()
+    {
+        HideAllButtons();
+        ShowButton();
+    }
+
+    private void HideAllButtons()
+    {
+        foreach (var b  in buttons)
+        {
+            b.transform.localScale = Vector3.zero;
+            b.SetActive(false);
+        }
+    }
+
+    private void ShowButton()
+    {
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            var b = buttons[i];
+            b.SetActive(true);
+            b.transform.DOScale(1, duration).SetDelay(i * delay).SetEase(ease);
+        }
+    }
+}
