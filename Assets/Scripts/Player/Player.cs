b485fee9602b7662195010dce93730ada102d554
César <cesar.romero.borges@gmail.com>
2025-05-15 18:23:37 -0300

    add Idle and Run animations into player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -20,7 +20,13 @@ public class Player : MonoBehaviour
     public float animationDuration = .3f;
     public Ease ease = Ease.OutBack;
 
+    [Header("Animation Player")]
+    public string boolRun = "Run";
+    public Animator animator;
+    public float playerSwipeDuration = .1f;
+
     private bool _isRunning = false;
+    private bool _isFacingRight = true;
 
     private void Update()
     {
@@ -32,13 +38,37 @@ public class Player : MonoBehaviour
     {
         _isRunning = Input.GetKey(KeyCode.LeftControl);
 
+        if (_isRunning)
+        {
+            animator.speed = 2;
+        }
+        else
+        {
+            animator.speed = 1;
+        }
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
+            animator.SetBool(boolRun, true);
             myRigidbody.velocity = new Vector2(_isRunning ? -speedRun : -speed, myRigidbody.velocity.y);
+            if (myRigidbody.transform.localScale.x != -1)
+            {
+                myRigidbody.transform.DOScaleX(-1, playerSwipeDuration);
+                _isFacingRight = false;            }
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
+            animator.SetBool(boolRun, true);
             myRigidbody.velocity = new Vector2(_isRunning ? speedRun : speed, myRigidbody.velocity.y);
+            if (myRigidbody.transform.localScale.x != 1)
+            {
+                myRigidbody.transform.DOScaleX(1, playerSwipeDuration);
+                _isFacingRight = true;
+            }
+        }
+        else
+        {
+            animator.SetBool(boolRun, false);
         }
 
         if (myRigidbody.velocity.x > 0)
@@ -57,9 +87,20 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             myRigidbody.velocity = Vector2.up * forceJump;
-            myRigidbody.transform.localScale = Vector2.one;
+            myRigidbody.transform.localScale = Vector3.one;
 
-            DOTween.Kill(myRigidbody.transform);
+            if (_isFacingRight)
+            {
+                jumpScaleX = (Mathf.Sign(jumpScaleX) == 1) ? jumpScaleX : -jumpScaleX;
+                myRigidbody.transform.localScale = Vector3.one;
+            }
+            else
+            {
+                jumpScaleX = (Mathf.Sign(jumpScaleX) == -1) ? jumpScaleX : -jumpScaleX;
+                myRigidbody.transform.localScale = new Vector3(-1, 1, 1);
+            }
+
+                DOTween.Kill(myRigidbody.transform);
 
             HandleScaleJump();
         }
