03cd95795b99b2b6bae1459e382941cede147bda
César <cesar.romero.borges@gmail.com>
2025-05-23 19:28:45 -0300

    add a script to make parts of scenery move in a constant speed

--- Assets/Scripts/Utils/TranslateGameObject.cs
+++ Assets/Scripts/Utils/TranslateGameObject.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class TranslateGameObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 5f;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
     }
 }
